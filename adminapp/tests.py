5a45709e0404e49761d1f4ba53395e9c3525fcae
Miquel <golobart@gmail.com>
2020-05-09 18:23:15 +0200

    Add basic test

--- adminapp/tests.py
+++ adminapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Resource, Calendar
+
+
+class ResourceModelTests(TestCase):
+
+    def test_resource_basics(self):
+        """
+        resource_basics() returns True if insertion is well done
+        """
+        calendar = Calendar(owner="oowwnneerr", name="ccaall__nnaammee",
+                            ini_day="2019-04-17", end_day="2020-12-14",
+                            template=True, open=True)
+        resource = Resource(name="tteesstt__nnaammee", calendar=calendar)
+        self.assertEqual(resource.name, "tteesstt__nnaammee")
+        self.assertEqual(resource.calendar, calendar)
+        self.assertEqual(calendar.name, "ccaall__nnaammee")
