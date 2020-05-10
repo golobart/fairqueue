e9d0d63feb7616a747bf5cddb6c841504ed35e27
Miquel <golobart@gmail.com>
2020-05-10 09:48:59 +0200

    more tests

--- adminapp/tests.py
+++ adminapp/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import Client
+from django.urls import reverse
 
 from .models import Resource, Calendar
 
+from django.contrib.auth.models import User
 
 class ResourceModelTests(TestCase):
 
@@ -18,3 +21,26 @@ class ResourceModelTests(TestCase):
         self.assertEqual(resource.name, "tteesstt__nnaammee")
         self.assertEqual(resource.calendar, calendar)
         self.assertEqual(calendar.name, "ccaall__nnaammee")
+
+class ViewTests(TestCase):
+
+    def test_view_basics(self):
+        """
+        test_view_basics() returns True if access to view with login required is well done
+        """
+        self.client = Client()
+        self.user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        login_res = self.client.login(username='john', password='johnpassword')
+
+
+#        login_res = self.client.login(username='miquel', password='marciano_1')
+        self.assertTrue(login_res)
+
+        response = self.client.get(reverse('adminapp:searchresources'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'input type="reset" value="Clear fields"', response.content)
+
+        self.client.logout()
+        response = self.client.get(reverse('adminapp:searchresources'))
+        self.assertEqual(response.status_code, 302)
+#        self.assertIn(b'not log', response.content)
