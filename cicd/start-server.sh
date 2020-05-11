36b46f41215742c6c4778610037dda6d087ace82
Miquel <golobart@gmail.com>
2020-05-11 20:17:30 +0200

    context Dockerfile

---
+++ cicd/start-server.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# start-server.sh
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+    (cd fairqueue; python manage.py createsuperuser --no-input)
+fi
+(cd fairqueue; python manage.py runserver 0.0.0.0:8000)
+#(. ./django_queue_venv/bin/activate; cd fairqueue; python manage.py runserver)
